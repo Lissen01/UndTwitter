35a6071182212800ca67f0b505449b2a4f78d507
Lissen01 <001lissen@gmail.com>
2020-07-02 19:52:29 -0300

    verPerfil

---
+++ routers/procesoToken.go
@@ -0,0 +1,46 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Lissen01/UndTwitter/bd"
+	"github.com/Lissen01/UndTwitter/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+/*Email valor de Email en todos los EndPoints*/
+var Email string
+
+/*IDUsuario es el ID devuelto del modelo, que se usara en todos los Endpoints*/
+var IDUsuario string
+
+/*ProcesoToken para extraer valores*/
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+	miClave := []byte("Ux_Uriel_DiseñoUx")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token Invalido")
+	}
+	return claims, false, string(""), err
+}
